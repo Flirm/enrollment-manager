bf31283a5f0ed853f76266bb2aa1c9600e72318b
Flirm <121270101+Flirm@users.noreply.github.com>
2024-06-08 18:14:32 -0300

    feat: create basic functionality of student service

--- src/main/java/com/franciscoramos/service/StudentService.java
+++ src/main/java/com/franciscoramos/service/StudentService.java
@@ -1,9 +1,49 @@
 package com.franciscoramos.service;
 
 import com.franciscoramos.dao.StudentDao;
+import com.franciscoramos.exception.EntityNotFoundException;
+import com.franciscoramos.exception.StudentEnrolledException;
+import com.franciscoramos.model.Student;
 import com.franciscoramos.util.DaoFactory;
+
+import java.util.List;
 
 public class StudentService
 {
     private final StudentDao studentDao = DaoFactory.getDao(StudentDao.class);
+
+    public Student create(Student student)
+    {
+        studentDao.create(student.getId(), student);
+        return student;
+    }
+
+    public Student remove(int id)
+    {
+        Student student = read(id);
+
+        if(student == null)
+            throw new EntityNotFoundException("Aluno inexistente.\n");
+
+        if(student.getClassrooms().isEmpty())
+            studentDao.remove(id);
+        else
+            throw new StudentEnrolledException("Este aluno esta matriculado em disciplinas e nao pode ser removido");
+
+        return student;
+    }
+
+    public Student read(int id)
+    {
+        Student student = studentDao.read(id);
+        if(student == null)
+            throw new EntityNotFoundException("Aluno inexistente.\n");
+        return student;
+    }
+
+    public List<Student> readAll()
+    {
+        List<Student> students = studentDao.readAll();
+        return students;
+    }
 }
