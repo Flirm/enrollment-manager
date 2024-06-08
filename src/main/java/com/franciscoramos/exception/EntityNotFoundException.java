6cabbaf40466d954fb215e519a7484ccee488cb6
Flirm <121270101+Flirm@users.noreply.github.com>
2024-06-08 18:12:44 -0300

    feat: create custom exceptions

---
+++ src/main/java/com/franciscoramos/exception/EntityNotFoundException.java
@@ -0,0 +1,5 @@
+package com.franciscoramos.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+    public EntityNotFoundException(String message) {super(message);}
+}
---
+++ src/main/java/com/franciscoramos/exception/StudentEnrolledException.java
@@ -0,0 +1,5 @@
+package com.franciscoramos.exception;
+
+public class StudentEnrolledException extends RuntimeException {
+    public StudentEnrolledException(String message) {super(message);}
+}
