025ac9045dc734d4f69b3dda44e7bd9fc45fb0a1
Flirm <121270101+Flirm@users.noreply.github.com>
2024-06-09 17:46:40 -0300

    refactor: isolating class attributes

--- src/main/java/com/franciscoramos/model/Classroom.java
+++ src/main/java/com/franciscoramos/model/Classroom.java
@@ -1,18 +1,19 @@
 package com.franciscoramos.model;
 
+import java.util.ArrayList;
+
 public class Classroom
 {
     private static int count = 0;
     private int id;
-    private String name;
     private String schoolTerm;
     private Discipline discipline;
     private Teacher teacher;
+    private ArrayList<Registry> registeredStudents;
 
     public Classroom(Discipline discipline, String schoolTerm, Teacher teacher)
     {
         this.id = Integer.parseInt(String.valueOf(count++) + String.valueOf(discipline.getId()) + schoolTerm);
-        this.name = discipline.getName();
         this.schoolTerm = schoolTerm;
         this.discipline = discipline;
         this.teacher = teacher;
@@ -20,14 +21,14 @@ public class Classroom
 
     public String toString()
     {
-        return "Nome: " + this.name + "\nProfessor: "+ this.teacher + "\nPeríodo: " + this.schoolTerm + "\nId: " + this.id + "\n";
+        return "Nome: " + this.discipline.getName() + "\nProfessor: "+ this.teacher + "\nPeríodo: " + this.schoolTerm + "\nId: " + this.id + "\n";
     }
 
     public int getId() {return this.id;}
     public void setId(int id) {this.id = id;}
 
-    public String getName() {return this.name;}
-    public void setName(String name) {this.name = name;}
+    public String getName() {return this.discipline.getName();}
+    public void setName(String name) {this.discipline.setName(name);}
 
     public String getSchoolTerm() {return this.schoolTerm;}
     public void setSchoolTerm(String schoolTerm) {this.schoolTerm = schoolTerm;}
