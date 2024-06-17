3b2aa030ff8bd6e30d68d6ff357098d53384632d
Flirm <121270101+Flirm@users.noreply.github.com>
2024-06-17 17:23:22 -0300

    feat: added needed exceptions

---
+++ src/main/java/com/franciscoramos/exception/DisciplineAlreadyCompleteException.java
@@ -0,0 +1,5 @@
+package com.franciscoramos.exception;
+
+public class DisciplineAlreadyCompleteException extends RuntimeException {
+    public DisciplineAlreadyCompleteException(String message) {super(message);}
+}
---
+++ src/main/java/com/franciscoramos/exception/InvalidGradeException.java
@@ -0,0 +1,5 @@
+package com.franciscoramos.exception;
+
+public class InvalidGradeException extends RuntimeException {
+    public InvalidGradeException(String message) {super(message);}
+}
