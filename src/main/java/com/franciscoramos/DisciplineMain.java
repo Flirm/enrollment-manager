78eeec0c6b0970a9240400838b602d2239a51455
Flirm <121270101+Flirm@users.noreply.github.com>
2024-07-13 19:45:48 -0300

    feat: case 5 list all pre-requisites from a discipline feat: case 6 get approval rate from the discipline in term

--- src/main/java/com/franciscoramos/DisciplineMain.java
+++ src/main/java/com/franciscoramos/DisciplineMain.java
@@ -2,11 +2,13 @@ package com.franciscoramos;
 
 import com.franciscoramos.exception.DisciplineWithClassroomsException;
 import com.franciscoramos.exception.EntityNotFoundException;
+import com.franciscoramos.model.Classroom;
 import com.franciscoramos.model.Discipline;
 import com.franciscoramos.service.DisciplineService;
 import corejava.Console;
 
 import java.util.List;
+import java.util.Objects;
 
 public class DisciplineMain
 {
@@ -27,7 +29,9 @@ public class DisciplineMain
             System.out.println("2. Remover disciplina\n");
             System.out.println("3. Listar todas as disciplinas\n");
             System.out.println("4. Alterar carga horária da disciplina\n");
-            System.out.println("5. Voltar\n");
+            System.out.println("5. Listar Pré-Requisitos de uma disciplina\n");
+            System.out.println("6. Quantidade de Alunos aprovados em uma disciplina\n");
+            System.out.println("7. Voltar\n");
             int result = Console.readInt("Digite um número entre 1 e 5: ");
 
             switch(result)
@@ -81,7 +85,40 @@ public class DisciplineMain
                         System.out.println(e.getMessage() + "\n");
                     }
                 }
-                case 5 -> loop = false;
+                case 5 ->{ //listar pre-requisitos
+                    int id = Console.readInt("Informe o id da disciplina: ");
+                    try
+                    {
+                        discipline = disciplineService.read(id);
+                        System.out.println("Pré-Requisitos da disciplina " + discipline.getName() + ": \n");
+                        for(Discipline d : discipline.getPreRequisites()){
+                            System.out.println(d + "\n");
+                        }
+                    }catch(EntityNotFoundException e)
+                    {
+                        System.out.println(e.getMessage() + "\n");
+                    }
+                }
+                case 6 ->{ //quant de alunos aprovados na disciplina no periodo x
+                    int id = Console.readInt("Informe o id da disciplina: ");
+                    try{
+                        discipline = disciplineService.read(id);
+                        String term = Console.readLine("Informe o período desejado: ");
+                        List<Classroom> classrooms = discipline.getClassrooms().stream().filter((c) -> Objects.equals(c.getSchoolTerm(), term)).toList();
+                        int total = 0;
+                        int approved = 0;
+                        for(Classroom classroom : classrooms){
+                            total += classroom.getStudentCount();
+                            approved += classroom.getApprovedStudentCount();
+                        }
+                        System.out.println("\nDisciplina: " + discipline.getName());
+                        System.out.println("Neste período foram aprovados " + approved + " em um total de " + total + " alunos.");
+                        System.out.println("Taxa de aprovação: " + ((float)approved/total)*100 + "%\n");
+                    }catch(EntityNotFoundException e){
+                        System.out.println(e.getMessage() + "\n");
+                    }
+                }
+                case 7 -> loop = false;
                 default -> System.out.println("Opção Inválida\n");
             }
         }
